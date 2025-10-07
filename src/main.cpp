bd1b50d8c4f10478590c525b4a84e87c343d1222
Alex Tepanosyan <aetepanosyan@gmail.com>
2025-10-07 14:44:09 +0000

    Changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,56 +1,33 @@
 #include <Arduino.h>
 #include <PicoMQTT.h>
 #include <GyverStepper2.h>
-#include <Wire.h>
 #include "I2Cdev.h"
-#include "MPU6050_6Axis_MotionApps20.h"
 
 // Пины, к которым подключены концевые выключатели
-#define END_CARRIAGE_CLOSE 27
+#define END_CARRIAGE_CLOSE 16
 #define END_CARRIAGE_FAR 32
-#define END_CARRIAGE_HOOK 33
 
-#define MOVE_STEP 23
-#define MOVE_DIR 14
+#define MOVE_STEP 2
+#define MOVE_DIR 4
 #define MOVE_EN 15
 
 // Шаговый двигатель движения
 GStepper2<STEPPER2WIRE> MoveStepper(2038, MOVE_STEP, MOVE_DIR, MOVE_EN);
 
 // Пины, к которым подключены шаговые двигатели
-#define STEP_CARRIAGE_1 19
-#define STEP_CARRIAGE_2 18
-#define STEP_CARRIAGE_3 5
-#define STEP_CARRIAGE_4 17
+#define CARRIAGE_STEP 14
+#define CARRIAGE_DIR 12
+#define CARRIAGE_EN 27
 
 // Шаговый двигатель каретки
-GStepper2<STEPPER4WIRE> CarriageStepper(2038, STEP_CARRIAGE_1, STEP_CARRIAGE_3, STEP_CARRIAGE_2, STEP_CARRIAGE_4);
+GStepper2<STEPPER2WIRE> CarriageStepper(2038, CARRIAGE_STEP, CARRIAGE_DIR, CARRIAGE_EN);
 
-#define TURN_STEP 16
-#define TURN_DIR 4
-#define TURN_EN 2
-
-// Шаговый двигатель поворота
-GStepper2<STEPPER2WIRE> TurnStepper(2038, TURN_STEP, TURN_DIR, TURN_EN);
-
-#define STEP_CABLE_1 13
-#define STEP_CABLE_2 12
-#define STEP_CABLE_3 25
-#define STEP_CABLE_4 26
+#define CABLE_STEP 25
+#define CABLE_DIR 26
+#define CABLE_EN 33 
 
 // Шаговый двигатель крюка
-GStepper2<STEPPER4WIRE> CableStepper(2038, STEP_CABLE_1, STEP_CABLE_3, STEP_CABLE_2, STEP_CABLE_4);
-
-// Пины, к которым подключен гироскоп
-#define GYRO_SDA 21
-#define GYRO_SCL 22
-
-// Стандартное положение гироскопа
-#define GYRO_ZERO_VAL 99.4
-
-// Минимальное и максимальное отклонение гироскопа
-#define GYRO_MIN_VAL 3
-#define GYRO_MAX_VAL 4
+GStepper2<STEPPER2WIRE> CableStepper(2038, CABLE_STEP, CABLE_DIR, CABLE_EN);
 
 // Максимальная скорость и ускорение двигателей
 const int maxSpeed = 1600;
@@ -60,12 +37,11 @@ const int acceleration = 50;
 PicoMQTT::Server mqtt;
 
 // Настройки подключения к WiFi
-const char* ssid     = "Rockus_PortCrane";
+const char* ssid     = "Rockus_ContainerCrane";
 const char* password = "Kochegar";
 
 // Функции для обработки сигналов с приложения
 void MoveMotor(const char* topic, const char* payload);
-void Turn(const char* topic, const char* payload);
 void MoveCarriage(const char* topic, const char* payload);
 void MoveHook(const char* topic, const char* payload);
 
@@ -75,14 +51,6 @@ double move_speed = 0;
 // Состояние нажатия концевиков
 bool far_end = false;
 bool close_end = false;
-bool hook_end = false;
-
-// Состояние гироскопа
-bool gyro_end = false;
-
-// Переменные для гироскопа
-MPU6050 mpu;
-uint8_t fifoBuffer[45]; 
 
 void setup() {
   // Разрешаем отладочный вывод
@@ -92,28 +60,20 @@ void setup() {
   pinMode(MOVE_STEP, OUTPUT);
   pinMode(MOVE_DIR, OUTPUT);
   pinMode(MOVE_EN, OUTPUT);
-  pinMode(STEP_CARRIAGE_1, OUTPUT);
-  pinMode(STEP_CARRIAGE_2, OUTPUT);
-  pinMode(STEP_CARRIAGE_3, OUTPUT);
-  pinMode(STEP_CARRIAGE_4, OUTPUT);
-  pinMode(TURN_STEP, OUTPUT);
-  pinMode(TURN_DIR, OUTPUT);
-  pinMode(TURN_EN, OUTPUT);
-  pinMode(STEP_CABLE_1, OUTPUT);
-  pinMode(STEP_CABLE_2, OUTPUT);
-  pinMode(STEP_CABLE_3, OUTPUT);
-  pinMode(STEP_CABLE_4, OUTPUT);
+  pinMode(CARRIAGE_STEP, OUTPUT);
+  pinMode(CARRIAGE_DIR, OUTPUT);
+  pinMode(CARRIAGE_EN, OUTPUT);
+  pinMode(CABLE_STEP, OUTPUT);
+  pinMode(CABLE_DIR, OUTPUT);
+  pinMode(CABLE_EN, OUTPUT);
 
   // Настройка пинов на ввод сигнала
   pinMode(END_CARRIAGE_CLOSE, INPUT_PULLUP);
   pinMode(END_CARRIAGE_FAR, INPUT_PULLUP);
-  pinMode(END_CARRIAGE_HOOK, INPUT_PULLUP);
 
   // Выставляем максимальную скорость и ускорение двигателей
   CarriageStepper.setMaxSpeed(maxSpeed);
   CarriageStepper.setAcceleration(acceleration);
-  TurnStepper.setMaxSpeed(maxSpeed);
-  TurnStepper.setAcceleration(acceleration);
   CableStepper.setMaxSpeed(maxSpeed);
   CableStepper.setAcceleration(acceleration);
   MoveStepper.setMaxSpeed(maxSpeed);
@@ -122,38 +82,8 @@ void setup() {
   // Задаём начальную скорость двигателям  pinMode(MOVE_DIR, OUTPUT);
 
   CarriageStepper.setSpeed(0);
-  TurnStepper.setSpeed(0);
   CableStepper.setSpeed(0);
   MoveStepper.setSpeed(0);
-
-  // Настройка гироскопа
-  byte error, address;
-  Wire.begin();
-
-  delay(1000);
-
-  // Сканируем устройства, чтобы найти гироскоп
-  Serial.println("Scanning...");
-  for(address = 1; address < 127; address++ )
-  {
-    Wire.beginTransmission(address);
-    error = Wire.endTransmission();
-  
-    // Если нашли гироскоп, то инициализируем его
-    if (error == 0)
-    {
-      Serial.print("I2C device found at address 0x");
-      if (address<16)
-        Serial.print("0");
-      Serial.print(address,HEX);
-      Serial.println("  !");
-      mpu = MPU6050(address);
-    }
-  }
-
-  mpu.initialize();
-  mpu.dmpInitialize();
-  mpu.setDMPEnabled(true);
 
   // Запуск MQTT-сервера и подключение к WiFi
   Serial.print("Запуск точки доступа");
@@ -165,77 +95,15 @@ void setup() {
 
   // Подписываемся на MQTT-топики
   mqtt.subscribe("move_carriage", MoveCarriage);
-  mqtt.subscribe("turn", Turn);
   mqtt.subscribe("move_motor", MoveMotor);
   mqtt.subscribe("move_hook", MoveHook);
 
   mqtt.begin();
 }
 
-// Переменные для гироскопа
-float ypr[3];
-uint32_t tmr;
-
-// Функция получения угла наклона крана
-void getAngle() {
-  // Опрашиваем 2 раза в секунду
-  if (millis() - tmr >= 500) { 
-    if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) {
-      Quaternion q;
-      VectorFloat gravity;
-      
-      // Считываем ускорение и угловую скорость
-      mpu.dmpGetQuaternion(&q, fifoBuffer);
-      mpu.dmpGetGravity(&gravity, &q);
-      // Эта функция рассчитывает текущий угол наклона крана
-      mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-
-      // Обновляем таймер
-      tmr = millis();
-      // Serial.println(ypr[1] * 57.3);
-    }
-
-    // Serial.println("GYRO: " + String(abs(ypr[1] * 57.3)) + " - " + String(GYRO_ZERO_VAL));
-  }
-}
-
-
 void loop() {
   // Обработка MQTT
   mqtt.loop();
-
-  // Получаем угол наклона крана
-  getAngle();
-
-  // Если угол наклона крана больше 4 градусов, то выключаем двигатели
-  double gyro_angle = abs(abs(ypr[1] * 57.3) - abs(GYRO_ZERO_VAL));
-
-  if(gyro_angle > GYRO_MAX_VAL) {
-    if(!gyro_end) {
-      TurnStepper.setSpeed(0);
-      TurnStepper.disable();
-
-      CarriageStepper.setSpeed(0);
-      CarriageStepper.disable();
-
-      CableStepper.setSpeed(0);
-      CableStepper.disable();
-
-      MoveStepper.setSpeed(0);
-      MoveStepper.disable();
-
-      Serial.println("Gyro angle error!");
-    }
-    gyro_end = true;
-  }
-  // Если угол наклона крана меньше 3 градусов, то выключаем двигатели
-  else if(gyro_angle < GYRO_MIN_VAL) {
-    if(gyro_end) {
-      Serial.println("Gyro angle OK!");
-    }
-    gyro_end = false;
-    
-  }
 
   // Проверка концевика на основании крана
   if(digitalRead(END_CARRIAGE_CLOSE) == 0) {
@@ -259,20 +127,6 @@ void loop() {
     far_end = false;
   }
 
-  // Проверка концевика на крюке крана
-  if(digitalRead(END_CARRIAGE_HOOK) == 0) {
-    if(!hook_end) {
-      CableStepper.setSpeed(0);
-      CarriageStepper.setSpeed(0);
-      CableStepper.disable();
-      CarriageStepper.disable();
-    }
-    hook_end = true;
-  } else {
-    hook_end = false;
-  }
-
-  TurnStepper.tick();
   CarriageStepper.tick();
   CableStepper.tick();
   MoveStepper.tick();
@@ -282,11 +136,6 @@ void loop() {
 void MoveMotor(const char* topic, const char* payload) {
   // Сохраняем скорость двигателя
   move_speed = String(payload).toDouble();
-
-  // Если угол наклона крана больше 4 градусов, то выключаем двигатели
-  if(gyro_end) {
-    move_speed = 0;
-  }
 
   // Двигаем двигатели
   MoveStepper.setTarget(move_speed*10);
@@ -303,28 +152,6 @@ void MoveMotor(const char* topic, const char* payload) {
 // Скорости шаговых двигателей
 double carriage_speed = 0;
 double hook_speed = 0;
-double turn_speed = 0;
-
-// Функция поворота крана
-void Turn(const char* topic, const char* payload) {
-  Serial.println("Turning: " + String(payload));
-  // Сохраняем скорость поворота
-  turn_speed = String(payload).toDouble();
-
-  // Если угол наклона крана больше 4 градусов, то выключаем двигатели
-  if(gyro_end) {
-    turn_speed = 0;
-  }
-
-  // Поворачиваем кран
-  TurnStepper.setTarget(turn_speed*10);
-  TurnStepper.setSpeed(turn_speed);
-  if(turn_speed != 0) {
-    TurnStepper.enable();
-  } else {
-    TurnStepper.disable();
-  }
-}
 
 // Функция движения крана
 void MoveCarriage(const char* topic, const char* payload) {
@@ -338,13 +165,7 @@ void MoveCarriage(const char* topic, const char* payload) {
   }
 
   // Если сработал концевик, то выключаем двигатели
-  if(hook_end) {
-    carriage_speed = carriage_speed > 0 ? 0 : carriage_speed;
-    hook_speed = hook_speed > 0 ? 0 : hook_speed;
-  }
-
-  // Если сработал концевик или угол наклона крана больше 4 градусов, то выключаем двигатели
-  if(far_end || gyro_end) {
+  if(far_end) {
     carriage_speed = carriage_speed > 0 ? 0 : carriage_speed;
   }
 
@@ -363,12 +184,6 @@ void MoveHook(const char* topic, const char* payload) {
   Serial.println("Moving hook: " + String(payload));
   // Сохраняем скорость двигателя
   hook_speed = String(payload).toDouble();
-
-  // Если сработал концевик или угол наклона крана больше 4 градусов, то выключаем двигатели
-  if(hook_end || gyro_end) {
-    hook_speed = hook_speed > 0 ? 0 : hook_speed;
-    carriage_speed = carriage_speed > 0 ? 0 : carriage_speed;
-  }
 
   // Двигаем двигатели
   CableStepper.setTarget(hook_speed*10);
